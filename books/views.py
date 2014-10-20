f1ef345c92504e14e82a34aed758cce285048dde
周玉勇 <zyy.bobo@gmail.com>
2014-10-20 17:08:45 +0800

    表单search

--- books/views.py
+++ books/views.py
@@ -10,16 +10,19 @@ def search_form(request):
 
 
 def search(request):
-    error = False
+    errors = []
     if 'q' in request.GET:
         q = request.GET['q']
         if not q:
-            error = True
-            return render_to_response('search_form.html', {'error': error})
+            errors.append('Enter a search term.')
+        elif len(q) > 20:
+            errors.append('Please enter at most 20 characters.')
         else:
             books = Book.objects.filter(title__icontains=q)
             return render_to_response(
                 'search_results.html',
-                {'books': books, 'query': q}
-            )
-    return render_to_response('search_form.html', {'error': error})
+                {'books': books, 'query': q})
+    return render_to_response(
+        'search_form.html',
+        {'errors': errors}
+        )
